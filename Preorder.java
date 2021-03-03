3042b54b70f86a6f583ada77ae8912d2b2adc7a1
Chunhou <liuchunhou@gmail.com>
2021-03-03 22:38:14 +0800

    Preorder implementation

--- Preorder.java
+++ Preorder.java
@@ -3,60 +3,60 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+    int val;
+    TreeNode left;
+    TreeNode right;
 }
 
 public class Preorder {
-	public List<Integer> recursivePreorder(TreeNode root) {
-		List<Integer> result = new ArrayList<>();
-		recursivePreorder(root, result);
-		return result;
-	}
-	private void recursivePreorder(TreeNode root, List<Integer> result) {
-		if(root==null) return;
-		result.add(root.val);
-		recursivePreorder(root.left, result);
-		recursivePreorder(root.right, result);
-	}
+    public List<Integer> recursivePreorder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        recursivePreorder(root, result);
+        return result;
+    }
+    private void recursivePreorder(TreeNode root, List<Integer> result) {
+        if(root==null) return;
+        result.add(root.val);
+        recursivePreorder(root.left, result);
+        recursivePreorder(root.right, result);
+    }
 
-	public List<Integer> nonrecursivePreorder(TreeNode root) {
-		Stack<TreeNode> stk = new Stack<>();
-		List<Integer> result = new Arraylist<>();
-		TreeNode p = root;
-		while(p!=null || !stk.isEmpty()) {
-			if(p==null) p=stk.pop();
-			result.add(p.val);
-			if(p.right!=null) stk.push(p.right);
-			p=p.left;
-		}
-		return result;
-	}
+    public List<Integer> nonrecursivePreorder(TreeNode root) {
+        Stack<TreeNode> stk = new Stack<>();
+        List<Integer> result = new Arraylist<>();
+        TreeNode p = root;
+        while(p!=null || !stk.isEmpty()) {
+            if(p==null) p=stk.pop();
+            result.add(p.val);
+            if(p.right!=null) stk.push(p.right);
+            p=p.left;
+        }
+        return result;
+    }
 
-	public List<Integer> morrisPreorder(TreeNode root) {
-		List<Integer> result = new ArrayList<>();
-		TreeNode p = root;
-		while(p!=null) {
-			if(p.left==null) {
-				result.add(p.val);
-				p=p.right;
-			} else {
-				TreeNode q=p.left;
-				while(q.right!=null && q.right!=p) q=q.right;
-				if(q.right==p) {
-					q.right=null;
-					p=p.right;
-				}
-				else {
-					q.right=p;
-					result.add(p.val);
-					p=p.left;
-				}
-			}
-		}
-		return result;
-	}
+    public List<Integer> morrisPreorder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        TreeNode p = root;
+        while(p!=null) {
+            if(p.left==null) {
+                result.add(p.val);
+                p=p.right;
+            } else {
+                TreeNode q=p.left;
+                while(q.right!=null && q.right!=p) q=q.right;
+                if(q.right==p) {
+                    q.right=null;
+                    p=p.right;
+                }
+                else {
+                    q.right=p;
+                    result.add(p.val);
+                    p=p.left;
+                }
+            }
+        }
+        return result;
+    }
 
 
 }
